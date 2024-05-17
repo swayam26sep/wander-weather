0f5e71dfb4ede0e6477e2f2081cedb6bc7021f6c
swayam26sep <146032267+swayam26sep@users.noreply.github.com>
2024-05-17 19:25:59 -0400

    figma

--- src/components/LocationField.jsx
+++ src/components/LocationField.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+const cityNameField = {
+  borderRadius: "15px",
+  borderColor: "#4C4DDS"
+  width: "334px",
+  height: "42px"
+}
 function LocationField({ location, setLocation, searchLocation }) {
   return (
     <div className="search">
       <input
+      style={cityNameField}
         value={location}
         onChange={(event) => setLocation(event.target.value)}
         onKeyPress={searchLocation}
